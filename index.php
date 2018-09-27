7944286e191775ee630268f1af2327bb0c9195ae
mikebottone <43454902+mikebottone@users.noreply.github.com>
2018-09-26 20:39:45 -0400

    created index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1> Team 7 Members</h1>
+<ul>
+<li> Rishabh Bahri</li>
+<li>Michael Bottone</li>
+<li> Yousuf Mustafa</li>
+<li>Kevin Lee</li>
+<li>Altamash Porbanderwala</li>	
+</ul>
+</body>
+</html>
