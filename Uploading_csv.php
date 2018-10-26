eaf06df8646efbf2526b0168d14d36c1a634f243
mikebottone <43454902+mikebottone@users.noreply.github.com>
2018-10-26 10:55:04 -0400

    Update Uploading_csv.php
    removed the specific connection and added the general one so everyone can connect to db

--- Uploading_csv.php
+++ Uploading_csv.php
@@ -1,43 +1,6 @@
 <?php
-$user = 'root';
-$password = 'root';
-$db = 'sinfo';
-$host = 'localhost';
-$port = 3308;
+include 'db-connection.php';
 
-//$link = mysqli_init();
-$todoAppMySQLConnection = new mysqli($host, $user, $password, $db, $port);
-
-$possibleLocalhosts = array('127.0.0.1', "::1");
-
-if(in_array($_SERVER['REMOTE_ADDR'], $possibleLocalhosts)) // If our REMOTE_ADDR is a localhost, do this:
-{ 
-  // Open a connection with our local database
-  $todoAppMySQLConnection = new mysqli($host, $user, $password, $db, $port);
-
- 
- 
-} 
-else // If our REMOTE_ADDR wasn't a localhost, we must be working remotely.
-{ 
-  // Parse our $databaseConnectURL so that we can pull out the key's we neeed
-  $parsedDatabaseConnectUrl = parse_url($databaseConnectURL);
-  $remote_host = $parsedDatabaseConnectUrl["host"];
-  $remote_username = $parsedDatabaseConnectUrl["user"];
-  $remote_password = $parsedDatabaseConnectUrl["pass"];
-  $remote_databaseName = substr($parsedDatabaseConnectUrl["path"], 1);
-
-  // Open a connection with our remote database
-  $todoAppMySQLConnection = mysqli_real_connect($remote_host, $remote_username, $remote_password, $remote_databaseName);
-  //notifies where connection made or not
-    if ($todoAppMySQLConnection->connect_errno) 
-    {
-    echo 'Failed to connect to MySQL: (' . $todoAppMySQLConnection->connect_errno . ') ' . $todoAppMySQLConnection->connect_error;
-    }
-    
-    //echo '<p>' . $todoAppMySQLConnection->host_info . '</p>';
-   
-} 
 //mysqli_select_db($todoAppMySQLConnection, "sinfo") or die ("no database");
 if(isset($_POST["submit"]))
 {
